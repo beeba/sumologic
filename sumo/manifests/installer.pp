30181430ea3cabeda320962d524e149e51ed49d0
Norman Choe <norman.choe@modolabs.com>
2015-10-16 15:37:01 -0400

    initial commit

--- sumo/manifests/installer.pp
+++
@@ -1,8 +0,0 @@
-class sumo::installer {
-
-  package { 'SumoCollector':
-     provider => 'rpm',
-     ensure => installed,
-     source => "https://collectors.sumologic.com/rest/download/rpm/64"
- }
-}
